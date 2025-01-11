bb480d81aeaa19823b9de54d0941379cc7d6bd2b
crossbrz8 <crossbrz8@hotmail.com>
2025-01-11 00:09:59 -0300

    there was not error for users which did not exist now fixed

--- src/controllers/Auth/login.controller.ts
+++ src/controllers/Auth/login.controller.ts
@@ -3,14 +3,25 @@ import { errorHandlerWrapper } from "../../utils";
 import { generateToken } from "../../utils/generate";
 import { comparePassword } from "../../utils/password";
 import httpStatus from "http-status";
+import { UnauthorizedError } from "../../errors/unauthorized.error";
 
 const loginHandler = async (req, res) => {
   const { email, password } = req.body;
   const findUser = await userService.getOneUser({ email });
-  if (!findUser) return null;
-  if (findUser.deletedAt) return null;
+  
+  if (!findUser) {
+    throw new UnauthorizedError("Invalid email or password");
+  }
+  
+  if (findUser.deletedAt) {
+    throw new UnauthorizedError("Account has been deleted");
+  }
+  
   const compare = await comparePassword(password, findUser.password);
-  if (!compare) return null;
+  if (!compare) {
+    throw new UnauthorizedError("Invalid email or password");
+  }
+  
   const token = generateToken(findUser.uuid);
   res.json({ token }).status(httpStatus.ACCEPTED);
 };
